409184fcdf983b3119c5814e87821a4890f6575a
CritteRo <critteras@gmail.com>
2021-12-03 15:21:34 +0200

    Bloodbowl start

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'CritteR / CritteRo'
+description 'GTA San Andreas Blood Bowl minigame working in FiveM.'
+
+client_scripts {
+    'client/cl_arena_interior.lua',
+}
+
+server_scripts {
+    ''
+}
